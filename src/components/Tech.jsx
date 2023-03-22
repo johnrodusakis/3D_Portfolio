09beab3312f5949e64473d83cf1530a3d70390f1
johnrod <68949961+johnrodusakis@users.noreply.github.com>
2023-03-22 11:24:59 +0200

    Completed Works Section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,7 +1,8 @@
+import React from "react";
+
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from '../hoc';
 import { technologies } from "../constants";
-
 
 const Tech = () => {
   return (
